5bf454ee3997b667ffafd2b5e7460eb920c6fac8
Leandro Pacheco <leandropacheco02@hotmail.com>
2024-05-11 14:53:39 -0300

    Tela de Login conforme o Design ( Falta ajustar o Height das Fontes)

--- FrontEnd/FrontEnd-TechChampions/src/Pages/login.jsx
+++ FrontEnd/FrontEnd-TechChampions/src/Pages/login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import './util.css';
-import './main.css';
-import './login.css';
+import '../Utils/css/util.css';
+import '../Utils/css/main.css';
+import '../Utils/css/login.css';
 import imagem from "./react.svg";
 
 const Login = () => {
@@ -11,24 +11,25 @@ const Login = () => {
 
   return (
     <div className="limiter">
-    <div id = "container">
-      <div id="inicio">
-        <img src={imagem} alt=""></img>
-        <p id="texto1">Fortalecendo laços, cultivando talentos, crescendo juntos!</p>
-      </div>
+      <div id="container">
+        <div id="inicio">
+          <img src={imagem} alt=""></img>
+          <p id="texto1">Fortalecendo laços, cultivando talentos, crescendo juntos!</p>
+        </div>
 
-      <div id="fim">
+        <div id="fim">
           <div className="container-login100">
             <div className="wrap-login100">
               <form className="login100-form validate-form">
                 <span id="textoWelcome" className="login100-form-title p-b-26">
-                Seja bem-vindo ao Sementes!
+                  Seja bem-vindo ao Sementes!
                 </span>
-                <span className="login100-form-title p-b-48">
-                <p id= "textoDescricao">Entre ou faça seu cadastro com a conta da empresa</p>
+                <span className="login100-form-title p-b-30">
+                  <p id="textoDescricao">Entre ou faça seu cadastro com a conta da empresa</p>
                 </span>
 
                 <div className="wrap-input100 validate-input" data-validate="Valid email is: a@b.c">
+                  <label className="senhaEmail" htmlFor="">E-mail</label>
                   <input className="input100" type="text" name="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)} />
@@ -39,23 +40,24 @@ const Login = () => {
                   <span className="btn-show-pass">
                     <i className="zmdi zmdi-eye"></i>
                   </span>
+                  <label className="senhaEmail" htmlFor="">Senha</label>
                   <input className="input100" type="password" name="pass" value={password}
                     onChange={(e) => setPassword(e.target.value)} />
                   <span className="focus-input100" data-placeholder="Password"></span>
                 </div>
 
                 <div className="checkbox-wrapper-29">
-                <label htmlFor="keepConnected" className="checkbox">
-                  <input
-                    className="checkbox__input"
-                    type="checkbox"
-                    id="keepConnected"
-                    checked={keepConnected}
-                    onChange={(e) => setKeepConnected(e.target.checked)}
-                  />
-                  <span className="checkbox__label"></span>
-                  <p>Manter-se conectado</p>
-                </label>
+                  <label htmlFor="keepConnected" className="checkbox">
+                    <input
+                      className="checkbox__input"
+                      type="checkbox"
+                      id="keepConnected"
+                      checked={keepConnected}
+                      onChange={(e) => setKeepConnected(e.target.checked)}
+                    />
+                    <span className="checkbox__label"></span>
+                    <p>Manter-se conectado</p>
+                  </label>
                 </div>
 
                 <div className="container-login100-form-btn">
@@ -69,17 +71,17 @@ const Login = () => {
 
                 <div className="container-login100-form-btn">
                   <div className="wrap-login100-form-btn">
-                    <div className="login100-form-bgbtn"></div>
-                    <button className="login100-form-btn">
+                    <div className="cadastro-bgbtn"></div>
+                    <button className="cadastro-btn">
                       Cadastrar Conta
                     </button>
                   </div>
                 </div>
 
-                
+
 
                 <div className="text-center p-t-20">
-                <a className="txt2" href="#">
+                  <a className="txt2" href="#">
                     Esqueceu sua Senha ?
                   </a>
                 </div>
